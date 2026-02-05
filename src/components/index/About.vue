89ee2873b993da7585d7b679ecdc4241f5a19c97
maliang <924818949@qq.com>
2026-02-05 10:55:42 +0800

    资源地址更新

--- src/components/index/About.vue
+++ src/components/index/About.vue
@@ -8,81 +8,73 @@
             <section class="mission">
               <div class="inner">
                 <div class="mission-content">
-                  <div class="title">我们的使命</div>
-                  <div class="sub">南生论坛，个人程序人生的点滴记录和时光储备。淡泊明志，宁静致远。珍惜原创，矢志不渝。</div>
-                </div>
-                <div class="vision-content">
-                  <div class="title">我们的愿景</div>
-                  <div class="sub">
-                    让所有迷失方向的同学找到前进的小路<br>
-                    让需要提升技术的同学能有所收获<br>
-                    让同学们有一个学习知识、分享技术的地方
+                  <div class="title">目的</div>
+                  <div class="sub"><span style="color: #cb2222">开源</span>之门敞，为迷途者照亮星光道；技术之海深，助进取者收获智慧果。
+                  </div>
+                  <div class="sub"><span style="color: #cb2222">运营</span>之志向，为南友们筑一方净土，笔耕生活琐碎，论道技术星辰，闲时悠然摸鱼，忙里偷闲赏花。
                   </div>
                 </div>
                 <div class="author-content">
                   <div class="title">作者</div>
                   <div class="sub">马亮南生（maliangnansheng）</div>
-                  <img title="微信" src="https://aid.nansin.top/default/wx-mlns.png" width="50%"/>
-                  <img title="QQ" src="https://aid.nansin.top/default/qq-mlns.jpg" width="50%"/>
+                  <img title="微信" src="https://76.nansin.top/bbs_ssm/wx-mlns.png" width="30%"
+                       style="margin-right: 10px"/>
+                  <img title="QQ" src="https://76.nansin.top/bbs_ssm/qq-mlns.jpg" width="30%"/>
                 </div>
                 <div class="code-content">
                   <div class="title">源码地址</div>
-                  <div class="sub">后端源码(GitHub)：<a target="_blank"
-                                                   href="https://github.com/maliangnansheng/bbs-springboot">bbs-springboot</a>
+                  <div class="sub">
+                    后端源码(GitHub)：
+                    <a target="_blank" href="https://github.com/maliangnansheng/bbs-springboot">bbs-springboot</a>
+                    、
+                    前端源码(GitHub)：
+                    <a target="_blank" href="https://github.com/maliangnansheng/bbs-vue-ui">bbs-vue-ui</a>
                   </div>
-                  <div class="sub">前端源码(GitHub)：<a target="_blank" href="https://github.com/maliangnansheng/bbs-vue-ui">bbs-vue-ui</a>
+                  <div class="sub">
+                    后端源码(Gitee)：
+                    <a target="_blank" href="https://gitee.com/maliangnansheng/bbs-springboot">bbs-springboot</a>
+                    、
+                    前端源码(Gitee)：
+                    <a target="_blank" href="https://gitee.com/maliangnansheng/bbs-vue-ui">bbs-vue-ui</a>
                   </div>
-                  <div class="sub">后端源码(Gitee)：<a target="_blank"
-                                                  href="https://gitee.com/maliangnansheng/bbs-springboot">bbs-springboot</a>
+                </div>
+                <div class="code-content">
+                  <div class="title">重点阅读</div>
+                  <div class="sub">
+                    深入了解：
+                    <a target="_blank" href="https://bbs.nansin.top/detail/171">南生运营的理念</a>
+                    、
+                    <a target="_blank" href="https://bbs.nansin.top/detail/679">如何进行南生论坛管理维护？</a>
+                    、
+                    <a target="_blank" href="https://bbs.nansin.top/detail/369">10分钟搞懂南生论坛</a>
+                    、
+                    <a target="_blank" href="https://bbs.nansin.top/detail/521">南生运营功能介绍</a>
+                    、
+                    <a target="_blank" href="https://bbs.nansin.top/detail/600">获取<span style="color: #cb2222">商用授权</span></a>
+                    、
+                    <a target="_blank" href="https://bbs.nansin.top/detail/641">其他<span style="color: #cb2222">技术方案</span></a>
+                    、
+                    <a target="_blank" href="https://bbs.nansin.top/detail/122">南生论坛<span style="color: #cb2222">日常迭代</span></a>
+                    、
+                    <a target="_blank" href="https://76.nansin.top/doc/ns_bbs_doc.docx">南生论坛设计文档</a>
                   </div>
-                  <div class="sub">前端源码(Gitee)：<a target="_blank" href="https://gitee.com/maliangnansheng/bbs-vue-ui">bbs-vue-ui</a>
+                  <div class="sub">
+                    部署教程：
+                    <a target="_blank" href="https://bbs.nansin.top/detail/158">【后端】安装部署教程</a>
+                    、
+                    <a target="_blank" href="https://bbs.nansin.top/detail/45">【前端】安装部署教程</a>
+                  </div>
+                  <div class="sub">
+                    视频教程：
+                    <a target="_blank" href="https://www.bilibili.com/video/BV1ThYQzfEvR">南生系列视频合集</a>
                   </div>
                 </div>
                 <div class="code-content">
                   <div class="title">学习路线</div>
                   <div class="sub">
                     <a target="_blank" href="https://bbs.nansin.top/detail/158">Java学习路线</a>
-                    <br>
+                    、
                     <a target="_blank" href="https://bbs.nansin.top/detail/159">前端学习路线</a>
-                  </div>
-                </div>
-                <div class="code-content">
-                  <div class="title">信息统一整理</div>
-                  <div class="sub">
-                    设计文档(论文)：
-                    <a target="_blank"
-                       href="https://76.nansin.top/doc/%E5%8D%97%E7%94%9F%E8%AE%BA%E5%9D%9B%E7%9A%84%E8%AE%BE%E8%AE%A1%E4%B8%8E%E5%AE%9E%E7%8E%B0.doc">南生论坛设计文档</a>
-                  </div>
-                  <div class="sub">
-                    日常迭代：
-                    <a target="_blank" href="https://bbs.nansin.top/detail/122">南生论坛日常迭代</a>
-                  </div>
-                  <div class="sub" style="color: #cb2222">
-                    【商用】需看：
-                    <a target="_blank" href="https://bbs.nansin.top/detail/171">南生运营的理念</a>
-                  </div>
-                  <div class="sub">
-                    安装部署教程：
-                    <br>
-                    <a target="_blank" href="https://bbs.nansin.top/detail/44">【后端】安装部署教程</a>
-                    <br>
-                    <a target="_blank" href="https://bbs.nansin.top/detail/45">【前端】安装部署教程</a>
-                  </div>
-                  <div class="sub">
-                    视频教程：
-                    <br>
-                    <a target="_blank"
-                       href="https://www.bilibili.com/video/BV1pK411C7T4/?spm_id_from=333.999.0.0&vd_source=d0a5d07985a7fbbfb889c874632343c6">南生论坛代码讲解视频</a>
-                    <br>
-                    <a target="_blank"
-                       href="https://www.bilibili.com/video/BV1he4y1C7rk/?spm_id_from=333.999.0.0&vd_source=d0a5d07985a7fbbfb889c874632343c6">南生论坛使用指南视频</a>
-                  </div>
-                  <div class="sub">
-                    南生闲聊：
-                    <br>
-                    <a target="_blank" href="https://76.nansin.top/jar/chat/ns-chat.zip">南生闲聊项目源码</a>
-                    <br>
-                    <a target="_blank" href="https://bbs.nansin.top/detail/71">南生闲聊教程</a>
                   </div>
                 </div>
               </div>
